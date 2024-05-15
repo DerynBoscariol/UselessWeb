48b912f38d6203efa477bc253c252e86c44fbe9a
Deryn Boscariol <derynb@rogers.com>
2024-05-15 15:17:14 -0400

    added all 7 gems in random positioning

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ window.onload = function (){
     
     function placeGem (){
         var genRandomX = randomNumber(0, 1300);
-        var genRandomY = randomNumber(0, 750);
+        var genRandomY = randomNumber(0, 700);
         var randomX = genRandomX.toString() + "px";
         var randomY = genRandomY.toString() + "px";
         gems[i].style.left = randomX;
