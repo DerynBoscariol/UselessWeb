681372ea6c9dcfa57742b120bcb3399190ed6178
Deryn Boscariol <derynb@rogers.com>
2024-05-15 14:54:33 -0400

    fixed functionality foor randomly placing 1 gem

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-
-var gem = document.getElementById("gem1");
-
-
 window.onload = function (){
-
+    var gem = document.getElementById("gem1");
 
     function randomNumber(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
       }
-
+    
     function placeGem (){
         var genRandomX = randomNumber(0, 1200);
         var genRandomY = randomNumber(0, 1200);
@@ -16,11 +12,8 @@ window.onload = function (){
         var randomY = genRandomY.toString() + "px";
         gem.style.left = randomX;
         gem.style.top = randomY;
-        console.log(randomX);
-        console.log(randomY);
-        console.log(genRandomX);
-        console.log(genRandomY);
+        console.log(randomX,randomY);
     }
-
-placeGem();
+    placeGem();
 }
+
