d75677d4dec176de338632e959f78834c21d2c8a
Deryn Boscariol <derynb@rogers.com>
2024-06-18 15:05:31 -0400

    all visuals done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
-window.onload = function (){
-    var gems = document.getElementsByClassName("gem");
 
+//var refreshBtn = document.getElementById("refreshBtn");
+const gems = [document.getElementsByClassName("gem")];
+
+window.onload = function (){
+  console.log(gems.length);
+  console.log(gems);
+   //Creating a random set of two numbers per gem
     for (let i = 0; i < gems.length; i++){
-    function randomNumber(min, max) {
+      function randomNumber(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
       }
-    
-    function placeGems (){
-        var genRandomX = randomNumber(0, 1300);
-        var genRandomY = randomNumber(0, 700);
-        var randomX = genRandomX.toString() + "px";
-        var randomY = genRandomY.toString() + "px";
-        gems[i].style.left = randomX;
-        gems[i].style.top = randomY;
-        console.log(randomX,randomY);
-    }
-    placeGems();
+    //Using said number to place gems at random x and y positions within the view window
+      var genRandomX = randomNumber(0, 1300);
+      var genRandomY = randomNumber(0, 700);
+      var randomX = genRandomX.toString() + "px";
+      var randomY = genRandomY.toString() + "px";
 
-}}
+      }
+
+      function placeGems (){
+        for (let i = 0; i < gems.length; i++){
+          gems[i].style.left = randomX;
+          gems[i].style.top = randomY;
+          console.log(randomX,randomY);
+        }
+  
+   }
+     //calling function to place gems
+  placeGems();
+   /*
+  //Recalling function to place gems when refresh button is clicked
+  refreshBtn.onclick = function()
+  { 
+      placeGems(); 
+  
+  } */
+  console.log(gems.length);
+}
+
 
