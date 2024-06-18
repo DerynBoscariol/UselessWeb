3530b1665a4c8f50556fd1363bde3b0cad0eed9b
Deryn Boscariol <derynb@rogers.com>
2024-06-18 18:52:01 -0400

    condensed code and fixed refresh button

--- js/script.js
+++ js/script.js
@@ -1,41 +1,35 @@
-
-//var refreshBtn = document.getElementById("refreshBtn");
-const gems = [document.getElementsByClassName("gem")];
-
 window.onload = function (){
+  var refreshBtn = document.getElementById("refreshBtn");
+  var getGems = document.getElementsByClassName("gem");
+  var gems = Array.from(getGems);
+
   console.log(gems.length);
-  console.log(gems);
-   //Creating a random set of two numbers per gem
-    for (let i = 0; i < gems.length; i++){
-      function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-      }
-    //Using said number to place gems at random x and y positions within the view window
+
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function placeGems(){
+    // Creating a random set of two numbers per gem
+    for (var i = 0; i < gems.length; i++){
+      // Using said number to place gems at random x and y positions within the view window
       var genRandomX = randomNumber(0, 1300);
       var genRandomY = randomNumber(0, 700);
       var randomX = genRandomX.toString() + "px";
       var randomY = genRandomY.toString() + "px";
+      gems[i].style.left = randomX;
+      gems[i].style.top = randomY;
+      console.log(randomX, randomY);
+      console.log(gems[i]);
+    }
+  }
 
-      }
-
-      function placeGems (){
-        for (let i = 0; i < gems.length; i++){
-          gems[i].style.left = randomX;
-          gems[i].style.top = randomY;
-          console.log(randomX,randomY);
-        }
-  
-   }
-     //calling function to place gems
   placeGems();
-   /*
-  //Recalling function to place gems when refresh button is clicked
-  refreshBtn.onclick = function()
-  { 
-      placeGems(); 
-  
-  } */
+
+  // Recalling function to place gems when refresh button is clicked
+  if (refreshBtn) {
+    refreshBtn.addEventListener("click", placeGems);
+  }
   console.log(gems.length);
 }
-
 
